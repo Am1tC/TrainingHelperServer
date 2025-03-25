03f4314ff494150c141b10502947b09dd98cdd78
User <User@LAB3-3>
2025-03-25 12:11:51 +0200

    backup restore

---
+++ TrainingHelperServer/DBScripts/BackupRestore.sql
@@ -0,0 +1,14 @@
+﻿﻿-- REPLACE YOUR DATABASE LOGIN AND PASSWORD IN THE SCRIPT BELOW
+
+Use master
+Go
+
+-- Create a login for the admin user
+CREATE LOGIN [TrainingHelperLogin] WITH PASSWORD ='123';
+Go
+
+--so user can restore the DB!
+ALTER ROLE db_owner ADD MEMBER [TrainingHelperUser];
+Go
+
+Create Database TrainingHelperDb;
